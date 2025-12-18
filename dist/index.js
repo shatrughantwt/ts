3ced4a90f1e0eee85eac97604ffad80052eda76a
shatrughan mishra <shatrughanm485@gmail.com>
2025-12-18 21:04:04 +0530

    learn ts

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return `Hello ${person}, Welcome to konoha`;
+}
+const username = "MOMOSHIKi";
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function greet(person: string): string{
+    return `Hello ${person}, Welcome to konoha`
+}
+
+const username: string = "MOMOSHIKi"
+console.log(greet(username));
