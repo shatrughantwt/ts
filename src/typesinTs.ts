a0517b2733c41179a59c8b92d2d7f8985382c1b0
shatrughan mishra <shatrughanm485@gmail.com>
2025-12-19 18:21:21 +0530

    learn annotation

---
+++ src/typesinTs.ts
@@ -0,0 +1,8 @@
+let drink: string = "boruto";
+
+let cups: number = Math.random() > 0.5 ? 10 : 5;
+
+let channelName: string = "boruto3";
+
+let waterFlavour: string = "water";
+waterFlavour = "only water";
